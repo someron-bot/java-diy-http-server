7037feb6b2d8b415ad3263d226bc017f73d3466d
someron <70521849+someron-bot@users.noreply.github.com>
2020-11-08 12:46:30 +0100

    chapter 04

---
+++ src/de/someron/diyHttpServer/ClientHandler.java
@@ -0,0 +1,17 @@
+package de.someron.diyHttpServer;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- src/de/someron/diyHttpServer/Main.java
+++ src/de/someron/diyHttpServer/Main.java
@@ -1,9 +1,32 @@
 package de.someron.diyHttpServer;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static int PORT = 80;
 
     public static void main(String[] args) {
         if(args.length == 1) PORT = Integer.parseInt(args[0]);
+        try {
+            listen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void listen() throws IOException {
+        ServerSocket server = new ServerSocket(PORT);
+        while(true) {
+            try {
+                Socket client = server.accept();
+                System.out.println("Connection from " + client.getInetAddress().getHostAddress());
+                client.getOutputStream().write("Hello World".getBytes());
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
